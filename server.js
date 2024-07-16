f4eff8c9f96e235187178993ef871e741c56ba73
rbsaketh <sbireddy@purdue.edu>
2024-07-15 21:57:57 -0400

    Author changes

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -23,8 +25,10 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started on port ${process.env.PORT || 3000}`);
