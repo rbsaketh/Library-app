eff54d5fbee31501428fd25624bc92c5f01a5f51
rbsaketh <sbireddy@purdue.edu>
2024-07-20 20:03:06 -0400

    Author route changes

--- models/author.js
+++ models/author.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const Book = require('./books')
 const authorScheme = new mongoose.Schema({
     name: {
         type: String,
@@ -7,5 +7,18 @@ const authorScheme = new mongoose.Schema({
     }
 })
 
+authorScheme.pre('remove', function(next) {
+    Book.find({ author: this.id}, (err, books) => {
+        if (err) {
+            next(err)
+        }
+        else if (books.length > 0) {
+            next(new Error('This author has books'))
+        }
+        else {
+            next()
+        }
+    })
+})
 
 module.exports = mongoose.model('Author', authorScheme)
